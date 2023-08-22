ce899d8b67fe2a32bdcd4fe67e16d462ea2f804e
philippOkie <kriukphilipp@gmail.com>
2023-08-22 13:44:54 +0300

    add adding new folders logic

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ let myFolders = []
 let folderId = 0
 let noteId = 0
 class Note {
-    constructor(title, description, dueDate, priority) {
-        this.title = title
+    constructor(noteTitle, description, dueDate, priority) {
+        this.noteTitle = noteTitle
         this.description = description
         this.dueDate = dueDate
         this.priority = priority
@@ -13,12 +13,40 @@ class Note {
     }
 }
 
+const folderContainer = document.getElementById('folderContainer')
 const currFolder = document.getElementById('currFolder')
 const submitForm = document.getElementById('controlFolder')
+const titleFolder = document.getElementById('titleFolder')
 
 submitForm.addEventListener('submit', (e) => {
     e.preventDefault()
+    let titleFolder = document.getElementById("titleFolder").value
+
+    const newFolder = document.createElement('div')
+    newFolder.classList.add('newFolder')
+    folderContainer.appendChild(newFolder)
+
+    const folderName = document.createElement("button")
+    folderName.innerHTML = titleFolder
+    folderName.classList.add('folderName')
+    newFolder.appendChild(folderName)
+    
+    const deleteBtn = document.createElement("button")
+    deleteBtn.innerHTML = "Delete"
+    deleteBtn.classList.add('delFolderBtn')
+    newFolder.appendChild(deleteBtn)
 })
+
+
+
+
+
+
+
+
+
+
+
 
 const todayFolder = document.getElementById('todayFolder')
 todayFolder.addEventListener('click', () => {
