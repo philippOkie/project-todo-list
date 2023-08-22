e92101916f0a8f643194c323f140cdf4d2e33a5d
philippOkie <kriukphilipp@gmail.com>
2023-08-22 13:15:40 +0300

    add Note class, I started to create logic of the app

--- src/index.js
+++ src/index.js
@@ -1,2 +1,31 @@
 import './styles/main.scss'
 
+let myFolders = []
+let folderId = 0
+let noteId = 0
+class Note {
+    constructor(title, description, dueDate, priority) {
+        this.title = title
+        this.description = description
+        this.dueDate = dueDate
+        this.priority = priority
+        this.id = noteId++
+    }
+}
+
+const currFolder = document.getElementById('currFolder')
+const submitForm = document.getElementById('controlFolder')
+
+submitForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+})
+
+const todayFolder = document.getElementById('todayFolder')
+todayFolder.addEventListener('click', () => {
+    currFolder.innerHTML = 'Today'
+})
+
+const upcomingFolder = document.getElementById('upcomingFolder')
+upcomingFolder.addEventListener('click', () => {
+    currFolder.innerHTML = 'Upcoming'
+})
